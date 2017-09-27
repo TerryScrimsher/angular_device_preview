8ef929e15fbde939c879caffcd6478a255fd94b5
Terry Scrimsher <terryscrimsher@gmail.com>
2017-09-27 10:38:11 -0700

    Font, broken link fixes

--- js/route.js
+++ js/route.js
@@ -3,26 +3,30 @@
  */
 var module = angular.module("myApp", ['ngRoute']);
 
-module.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-        .when(':param1', {
-            templateUrl: 'devices.html',
-            controller: 'RoutingController'
-        })
-        .otherwise({
-          templateUrl: 'devices.html',
-          controller: 'RoutingController'
-        });
-}]);
+// module.config(['$routeProvider', function ($routeProvider) {
+//     $routeProvider
+//         .when('/desktop', {
+//             templateUrl: 'desktop.html',
+//             controller: 'RoutingController'
+//         })
+//         .otherwise({
+//           templateUrl: 'index.html',
+//           controller: 'RoutingController'
+//         });
+// }]);
 
 module.controller("RoutingController", ['$scope', '$sce', '$location', function ($scope, $sce, $location) {
 
   var url = $location.path().split('/');
   if (url[1]) {
     $scope.firstParameter = "https://www." + url[1];
+    $scope.routeURL = "#/" + url[1];
   } else {
     $scope.firstParameter = "https://www." + "logicalposition.com";
+    $scope.routeURL = "";
   }
-  
+
   $scope.url = $sce.trustAsResourceUrl($scope.firstParameter);
+
+
 }]);
